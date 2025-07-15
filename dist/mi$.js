733228e02b11e708651570fbfe1eb6b34d9ab61a
DeiV-DeiV <daddylopez1999@gmail.com>
2025-07-15 17:35:27 -0400

    Actualización automática

--- dist/mi$.js
+++ dist/mi$.js
@@ -74,7 +74,7 @@ var mi$ = (function (exports) {
       };
       const onMouseMove = function (e) {
           if(!isDraggable) return
-          currentX = e.clientX -offsetX;
+          currentX = e.clientX - offsetX;
         UpdatePos();
       };
       const onMouseUp = function () {};
