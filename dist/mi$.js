1df3da7f6bf1372a2e231372a96f4931c82b0796
DeiV-DeiV <daddylopez1999@gmail.com>
2025-07-15 21:19:55 -0400

    Actualización automática

--- dist/mi$.js
+++ dist/mi$.js
@@ -138,7 +138,7 @@ var mi$ = (function (exports) {
     },
 
     addClass(classname){
-
+      
     },
 
     ...ayax,
--- src/dom.js
+++ src/dom.js
@@ -42,7 +42,7 @@ export const metodos = Object.freeze({
   },
 
   addClass(classname){
-
+    
   },
 
   ...ayax,
