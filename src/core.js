ea47d386eaf902af1d69b1b87dfcc2aadc7918d5
DeiV-DeiV <daddylopez1999@gmail.com>
2025-07-16 17:13:30 -0400

    Actualización automática

--- src/core.js
+++ src/core.js
@@ -10,7 +10,7 @@ const DEV_MODO = false; // usar FALSE para produccion
 
 const proxyCache = new WeakMap()
 
-const getProxy = (el)=>{
+export const getProxy = (el)=>{
   if (!proxyCache.has(el)) {
     const proxy = usarProxyProtegido([el]);
     proxyCache.set(el, proxy);
