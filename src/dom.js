1e3db8754ebbc1384b6b6cd6d816033f439c06cb
DeiV-DeiV <daddylopez1999@gmail.com>
2025-07-16 17:13:35 -0400

    Actualización automática

--- src/dom.js
+++ src/dom.js
@@ -24,7 +24,7 @@ export const metodos = Object.freeze({
   css(style = {}) {
     this._forEach((el) => {
       for (let [prop, val] of Object.entries(style)) {
-        getProxy()
+        et()
         el.style[prop] = val;
       }
     });
