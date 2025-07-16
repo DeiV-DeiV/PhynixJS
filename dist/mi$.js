1758b80cbf768078a217b7070bae0557403ca904
DeiV-DeiV <daddylopez1999@gmail.com>
2025-07-15 21:05:13 -0400

    Actualización automática

--- dist/mi$.js
+++ dist/mi$.js
@@ -145,21 +145,32 @@ var mi$ = (function (exports) {
   // consume menos memoria, mi punto es ese
 
 
-  const aplicarMetodos = (nodoArray) => {
-    // evita usar proxy
-    for (const nameMetodo of Object.keys(metodos)) {
-      Object.defineProperty(nodoArray, nameMetodo, {
-        value: metodos[nameMetodo],
-        writable: false,
-        configurable: false,
-        enumerable: false,
-      });
-    }
+  const usarProxyProtegido = (nodoArray) => {
+    return new Proxy(nodoArray, {
+      get(target, prop) {
+        if (prop in target) return target[prop];
+        if (prop in metodos) {
+          const fn = metodos[prop];
+          Object.defineProperty(target, prop, {
+            value: fn,
+            writable: false,
+            configurable: false,
+            enumerable: false,
+          });
 
-    // Previene agregar nuevas propiedades
-    return nodoArray
-    // return Object.freeze(nodoArray);
-    // Object.freeze(Persona.prototype);
+          return fn;
+        }
+
+        return undefined;
+      },
+
+      set(target, prop, value) {
+        if (prop in metodos)
+          throw new Error(`No puedes sobreescribir el metodo ${prop}`);
+        target[prop] = value;
+        return true;
+      },
+    });
   };
 
   const mi$ = (selector) => {
@@ -171,7 +182,8 @@ var mi$ = (function (exports) {
     if (typeof selector === "string") {
       const elements = Array.from(document.querySelectorAll(selector));
       
-      return aplicarMetodos(elements);
+      return usarProxyProtegido(elements)
+        ;
     }
 
     return [];
