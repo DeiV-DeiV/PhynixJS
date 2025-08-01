90d15bbbfac4265edb8de1b78388490e691b53ac
DeiV-DeiV <daddylopez1999@gmail.com>
2025-08-01 17:58:23 -0400

    Ultima Actualización

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { evGlobales } from "./metodosGlobales/eventsGlobals.js";
 // exportacion global y modulo
 window.$ = $;
 window.$$ = $$;
+window.x
 
 // Exportacion metodosGlobales |metodo y global|
 
