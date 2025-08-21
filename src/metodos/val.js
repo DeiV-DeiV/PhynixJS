5d6c1e0c0ef11551a038cc812ae372e0ed0d95c1
DeiV-DeiV <daddylopez1999@gmail.com>
2025-08-20 20:04:10 -0400

    Ultima Actualización

--- src/metodos/val.js
+++ src/metodos/val.js
@@ -1,10 +1,9 @@
 // metodos/val.js
 
 function val(val) {
- 
-    for (const el of this) {
-      el.value = val;
-    }
-  
+  for (const el of this) {
+    el.value = val;
+  }
+
   return this;
 }
