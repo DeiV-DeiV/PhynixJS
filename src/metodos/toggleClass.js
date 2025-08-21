c0acabd1bbeece58420581e2c0c364cb06085470
DeiV-DeiV <daddylopez1999@gmail.com>
2025-08-20 21:18:25 -0400

    Ultima Actualización

--- src/metodos/toggleClass.js
+++ src/metodos/toggleClass.js
@@ -1,6 +1,6 @@
 // src/metodos/toggleClass.js
 export function toggleClass(classname) {
-  validate
+  Va
   const clase = classname.trim().split(/\s+/);
   this._forEach((el) => {
     for (let cls of clase) el.classList.toggle(cls);
