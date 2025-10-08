9a2efed385dfd233025041af4f6b1e233a4d0736
DeiV-DeiV <daddylopez1999@gmail.com>
2025-10-08 02:02:43 -0300

    Ultima Actualización

--- src/modules/validate/validate.js
+++ src/modules/validate/validate.js
@@ -5,33 +5,30 @@ import { validator } from "./validator.js";
 
 export function validate(args = {}) {
   let errorList = []; // acumulador de errores
-  const exist = document.querySelector('.validate')
-    if(exist)exist.remove()
-  
+  const exist = document.querySelector(".validate");
+  if (exist) exist.remove();
+
   try {
+    for (const [key, value] of Object.entries(args)) {
+      if (!validator[key]) throw new Error("Key Invalido");
 
-  for (const [key, value] of Object.entries(args)) {
-    if (!validator[key]) throw new Error("Key Invalido");
-    
-    const { fn, msg } = validator[key];
+      const { fn, msg } = validator[key];
 
-    if (!fn(value)) {
-      errorList.push({ msg, value: JSON.stringify(value ?? " ") });
+      if (!fn(value)) {
+        errorList.push({ msg, value: JSON.stringify(value ?? " ") });
+      }
     }
-  }
 
- if (errorList.length > 0) {
+    if (errorList.length > 0) {
       $formValidate(errorList)();
       return false;
     }
-    
-
   } catch (xx) {
-    
-    console.error('Error Validate:', (errorList.length > 0) ?$formValidate(errorList)(): xx)
+    console.error(
+      "Error Validate:",
+      errorList.length > 0 ? $formValidate(errorList)() : xx
+    );
   }
-
-
 }
 
 window.Validate = validate;
