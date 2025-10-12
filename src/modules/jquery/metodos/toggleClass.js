5088f4b2517a9ddadbd198445b0fa884a4cc7631
DeiV-DeiV <daddylopez1999@gmail.com>
2025-10-12 03:45:51 -0300

    Ultima Actualización

--- src/modules/jquery/metodos/toggleClass.js
+++ src/modules/jquery/metodos/toggleClass.js
@@ -5,6 +5,7 @@ import { Validate } from "../../validate/Validate.js";
 
 export function toggleClass(classname) {
   Validate('toggleClass',{string:classname})
+  
   const clase = classname.trim().split(/\s+/);
   this._forEach((el) => {
     for (let cls of clase) el.classList.toggle(cls);
