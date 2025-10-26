6b51e31a281f7554b3d410445a2c677d3cd558d5
DeiV-DeiV <daddylopez1999@gmail.com>
2025-10-26 10:46:50 -0300

    Ultima Actualización

--- src/modules/jquery/metodosGlobales/On.js
+++ src/modules/jquery/metodosGlobales/On.js
@@ -9,7 +9,7 @@ export function On(ev = "", obj = {}) {
     for (let i = 0; i < entries.length; i++) {
       const [selector, callback] = entries[i];
       const target = e.target.closest(selector);
-      if (target) callback.call($(target), e);
+      if (target) return callback.call($(target), e);
     }
   });
 }
