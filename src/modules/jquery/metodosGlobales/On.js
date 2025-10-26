21ce267f10d7cbb67902df80a88a04044abda31a
DeiV-DeiV <daddylopez1999@gmail.com>
2025-10-25 21:49:03 -0300

    Ultima Actualización

--- src/modules/jquery/metodosGlobales/On.js
+++ src/modules/jquery/metodosGlobales/On.js
@@ -1,18 +1,14 @@
 // src/metodosGlobales/On.js
 
-import { Validate } from "../../validate/Validate.js";
-
-export function On(ev='', obj = {}) {
-
+export function On(ev = "", obj = {}) {
+  const entries = Object.entries(obj);
   document.body.addEventListener(ev, function (e) {
-    for (const [selector, callback] of Object.entries(obj)) {
-      Validate({string:selector, callback})
+    for (let i = 0; i < entries.length; i++) {
+      const [selector, callback] = entries[i];
       const target = e.target.closest(selector);
       if (target) callback.call(target, e);
     }
   });
-
- 
 }
 
 window.On = On;
